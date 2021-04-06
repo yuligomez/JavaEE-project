38cc3c02a4660e84abead8b9197e3a17b91e9265
yuligomez <yulianagomezsilva@gmail.com>
2021-04-06 15:53:04 +0200

    creo TestPersonaServiceRS.java
    creo TestPersonaServiceRS  y defino variables que voy a utilizar

---
+++ webservices REST/ClienteSgaRestWs/src/main/java/test/TestPersonaServiceRS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import domain.Persona;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class TestPersonaServiceRS {
+    //variables que voy a utilizar 
+    private static final String URL_BASE = "http://localhost:8080/sga-jee-web/webservice";
+    private static Client cliente;
+    private static WebTarget webTarget;
+    private static Persona persona;
+    private static List<Persona> personas;
+    private static Invocation.Builder invocationBuilder;
+    private static Response response;
+}
