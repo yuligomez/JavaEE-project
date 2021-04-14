4bbb6442ecb3e49ebe2dd506a34c39e635b82f26
yuligomez <yulianagomezsilva@gmail.com>
2021-04-14 16:12:25 +0200

    add Manejo de archivos ANT JAVA PROYECT

---
+++ ManejodeArchivosConAnt/ManejoDeArchivos/src/manejoarchvios/ManejoArchivos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoarchvios;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author yulia
+ */
+public class ManejoArchivos {
+    
+    public static void crearArchivo (String nombreArchivo)
+    {
+        File archivo = new File (nombreArchivo);
+        PrintWriter salida;
+        try {
+            salida = new PrintWriter (archivo);
+            salida.close();
+            System.out.println("Se ha creado el archivo");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+       
+       
+        
+    }
+    
+}
---
+++ ManejodeArchivosConAnt/ManejoDeArchivos/test/Test.java
@@ -0,0 +1,12 @@
+
+import static manejoarchvios.ManejoArchivos.crearArchivo;
+
+public class Test {
+    
+    public static void main (String [] args)
+    {
+        String nombreArchivo = "prueba.txt";
+        crearArchivo(nombreArchivo);
+    }
+    
+}
