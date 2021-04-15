d69e5dcf0ed9565cfd7f21fed8a0ee34112277a3
yuligomez <yulianagomezsilva@gmail.com>
2021-04-15 10:03:31 +0200

    agrego funcion anexarContenido

--- ManejodeArchivosConAnt/ManejoDeArchivos/src/manejoarchvios/ManejoArchivos.java
+++ ManejodeArchivosConAnt/ManejoDeArchivos/src/manejoarchvios/ManejoArchivos.java
@@ -7,6 +7,8 @@ package manejoarchvios;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,12 +33,26 @@ public class ManejoArchivos {
        
     } 
     
-     public static void escrbibirArchivo (String nombreArchivo , String contenido)
+    public static void escrbibirArchivo (String nombreArchivo , String contenido)
     {
         File archivo = new File (nombreArchivo);
         PrintWriter salida;
         try {
             salida = new PrintWriter (archivo);
+            salida.println(contenido);
+            salida.close();
+            System.out.println("Se ha escrito el archivo");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+       
+    } 
+    
+     public static void anexarContenido (String nombreArchivo , String contenido) throws IOException
+    {
+        File archivo = new File (nombreArchivo);
+        try {
+            PrintWriter salida = new PrintWriter (new FileWriter(archivo, true));
             salida.println(contenido);
             salida.close();
             System.out.println("Se ha escrito el archivo");
--- ManejodeArchivosConAnt/ManejoDeArchivos/test/Test.java
+++ ManejodeArchivosConAnt/ManejoDeArchivos/test/Test.java
@@ -1,14 +1,17 @@
 
+import java.io.IOException;
+import static manejoarchvios.ManejoArchivos.anexarContenido;
 import static manejoarchvios.ManejoArchivos.crearArchivo;
 import static manejoarchvios.ManejoArchivos.escrbibirArchivo;
 
 public class Test {
     
-    public static void main (String [] args)
+    public static void main (String [] args) throws IOException
     {
         String nombreArchivo = "prueba2.txt";
         crearArchivo(nombreArchivo);
-        escrbibirArchivo("prueba2.txt", "hola mundooooooooo");
+        anexarContenido("prueba2.txt", "hola mundooooooooo");
+        anexarContenido("prueba2.txt", "chau mundo");
     }
     
 }
