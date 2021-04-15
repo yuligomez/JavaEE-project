63b48f66339c588e1bf82f8a0bda3f34992de77a
yuligomez <yulianagomezsilva@gmail.com>
2021-04-15 09:53:38 +0200

    agrego funcion escrbibirArchivo

--- ManejodeArchivosConAnt/ManejoDeArchivos/src/manejoarchvios/ManejoArchivos.java
+++ ManejodeArchivosConAnt/ManejoDeArchivos/src/manejoarchvios/ManejoArchivos.java
@@ -29,8 +29,22 @@ public class ManejoArchivos {
             ex.printStackTrace(System.out);
         }
        
+    } 
+    
+     public static void escrbibirArchivo (String nombreArchivo , String contenido)
+    {
+        File archivo = new File (nombreArchivo);
+        PrintWriter salida;
+        try {
+            salida = new PrintWriter (archivo);
+            salida.println(contenido);
+            salida.close();
+            System.out.println("Se ha escrito el archivo");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
        
-        
-    }
+    } 
+    
     
 }
--- ManejodeArchivosConAnt/ManejoDeArchivos/test/Test.java
+++ ManejodeArchivosConAnt/ManejoDeArchivos/test/Test.java
@@ -1,12 +1,14 @@
 
 import static manejoarchvios.ManejoArchivos.crearArchivo;
+import static manejoarchvios.ManejoArchivos.escrbibirArchivo;
 
 public class Test {
     
     public static void main (String [] args)
     {
-        String nombreArchivo = "prueba.txt";
+        String nombreArchivo = "prueba2.txt";
         crearArchivo(nombreArchivo);
+        escrbibirArchivo("prueba2.txt", "hola mundooooooooo");
     }
     
 }
